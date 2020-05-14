59605946b4ba26802ae92a6ad758edfc6fa9c445
Jens <jkh@beckmann-partner.de>
2020-05-14 22:46:06 +0200

    Über GitHub Packages veröffentlichen

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,42 +1,73 @@
 #!/bin/bash
 set -e
 
+java -version
+mvn -v
+
 cd $GITHUB_WORKSPACE
+
+echo "#################################################"
+echo "Create Maven settings"
+
+cat > settings.xml <<EOF
+<!--
+  ~ Copyright 2020 Beckmann & Partner CONSULT
+  ~
+  ~ Licensed under the Apache License, Version 2.0 (the "License");
+  ~ you may not use this file except in compliance with the License.
+  ~ You may obtain a copy of the License at
+  ~
+  ~     http://www.apache.org/licenses/LICENSE-2.0
+  ~
+  ~ Unless required by applicable law or agreed to in writing, software
+  ~ distributed under the License is distributed on an "AS IS" BASIS,
+  ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  ~ See the License for the specific language governing permissions and
+  ~ limitations under the License.
+  -->
+
+<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
+          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
+                      http://maven.apache.org/xsd/settings-1.0.0.xsd">
+
+    <activeProfiles>
+        <activeProfile>github</activeProfile>
+    </activeProfiles>
+
+    <profiles>
+        <profile>
+            <id>github</id>
+            <repositories>
+                <repository>
+                    <id>central</id>
+                    <url>https://repo1.maven.org/maven2</url>
+                    <releases><enabled>true</enabled></releases>
+                    <snapshots><enabled>true</enabled></snapshots>
+                </repository>
+                <repository>
+                    <id>github</id>
+                    <name>GitHub BeckmannundPartnerCONSULT Apache Maven Packages</name>
+                    <url>https://maven.pkg.github.com/BeckmannundPartnerCONSULT/DocumentationTree</url>
+                </repository>
+            </repositories>
+        </profile>
+    </profiles>
+
+    <servers>
+        <server>
+            <id>github</id>
+            <username>beckDev</username>
+            <password>${GITHUB_TOKEN}</password>
+        </server>
+    </servers>
+</settings>
+EOF
 
 echo "#################################################"
 echo "Starting the Maven Action"
 
-sh -c "mvn clean install"
-
-echo "#################################################"
-echo "Maven build done"
-
-echo "#################################################"
-echo "Now publishing"
-SOME_TOKEN=${GITHUB_TOKEN}
-
-USER_NAME="${GITHUB_ACTOR}"
-MAIL="${GITHUB_ACTOR}@users.noreply.github.com"
-
-echo "Set user data."
-git config user.name "${USER_NAME}"
-git config user.email "${MAIL}"
-
-FILE=release
-if [ ! -d "$FILE" ]; then
-    mkdir $FILE
-fi
-
-cp target/documentationtree-*-jar-with-dependencies.jar release
-
-echo "Add all files."
-git add release/*.jar
-git status
-
-git diff-index --quiet HEAD || echo "Commit changes." && git commit -m 'Maven build from Action' && echo "Push." && git push
+sh -c "mvn deploy --settings settings.xml"
 
 echo "#################################################"
 echo "Published"
-
-
-echo "WOW!"
