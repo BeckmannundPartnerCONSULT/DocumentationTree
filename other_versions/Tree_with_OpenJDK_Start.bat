e643de5f6da11c044816793fc9272ebff7f7219d
BrunoKaiser <64803261+BrunoKaiser@users.noreply.github.com>
2021-10-22 17:01:55 +0200

    add Tree_with_OpenJDK_Start.bat
    start documentationtree-13.50-jar-with-dependencies.jar with OpenJDK

---
+++ other_versions/Tree_with_OpenJDK_Start.bat
@@ -0,0 +1,4 @@
+C:\jdk-16.0.2\bin\java.exe --module-path C:\javafx-sdk-17.0.1\lib --add-modules javafx.controls -jar C:\Tree\documentationtree-13.50-jar-with-dependencies.jar
+rem JavaFX to be downloaded from https://gluonhq.com/products/javafx/ with Windows 17.0.1 x64 SDK
+rem C:\Tree\documentationtree-13.50-jar-with-dependencies.jar from https://github.com/BeckmannundPartnerCONSULT/DocumentationTree/packages/227168
+
