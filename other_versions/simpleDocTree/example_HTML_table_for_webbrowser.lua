a398c373a03bd15f111ac3101f5aabe328b317a5
BrunoKaiser <64803261+BrunoKaiser@users.noreply.github.com>
2021-10-22 09:40:17 +0200

    add example_HTML_table_for_webbrowser.lua

---
+++ other_versions/simpleDocTree/example_HTML_table_for_webbrowser.lua
@@ -0,0 +1,24 @@
+
+
+TextHTMLtable={}
+
+TextHTMLtable[#TextHTMLtable+1]=[[
+
+<!DOCTYPE html><head></head><html><body>
+This is a first page.
+</body></html>
+]]
+TextHTMLtable[#TextHTMLtable+1]=[[
+
+<!DOCTYPE html><head></head><html><body>
+This is a second page.
+</body></html>
+]]
+TextHTMLtable["Erklärungen"]=[[
+
+<!DOCTYPE html><head></head><html><body>
+This is a description page.
+</body></html>
+]]
+
+
