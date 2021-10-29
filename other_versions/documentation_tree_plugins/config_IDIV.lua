cc459b03c4ce1358b98b8ae68a63c7d0fdbb21fe
BrunoKaiser <64803261+BrunoKaiser@users.noreply.github.com>
2021-10-29 08:28:07 +0200

    add config_IDIV.lua
    add config_IDIV.lua configuration of interactive dynamic table of contents

---
+++ other_versions/documentation_tree_plugins/config_IDIV.lua
@@ -0,0 +1,15 @@
+
+--1. size of buttons
+button_logo.size="23x20"
+button_save_lua_table.size="95x20"
+button_search.size="55x20"--"85x20"
+button_replace.size="85x20"--"105x20"
+button_compare.size="85x20"--"105x20"
+button_copy_title.size="105x20"--"105x20"
+button_edit_treescript.size="75x20"--"105x20"
+button_statisticsupdate.size="65x20"--"105x20"
+button_edit_treestatistics.size="75x20"--"105x20"
+button_logo2.size="23x20"
+tree.size="400x200"--"300x200"
+textfield1.size="527x120"--"340x120"
+maindlg.size = 'FULLxFULL'
