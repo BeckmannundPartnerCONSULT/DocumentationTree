4b28244c898e83c836e188a14f70a766f6a1b4ec
BrunoKaiser <64803261+BrunoKaiser@users.noreply.github.com>
2022-01-26 11:41:14 +0100

    add files simple_tree_screenshots.lua
    add files simple_tree_screenshots.lua

---
+++ other_versions/Tree_screenshots/screenshot_tree.lua
@@ -0,0 +1,7 @@
+tree_from_csv={branchname="Baum erstellt am: Thu Oct 21 20:08:36 2021",
+"C:\\Tree\\Tree_screenshots\\01_github.png",
+"C:\\Tree\\Tree_screenshots\\02_download.png",
+"C:\\Tree\\Tree_screenshots\\03_Eingabe_in_Editor.png",
+"C:\\Tree\\Tree_screenshots\\04_Speichern_unter_Dateinamen.png",
+
+}
---
+++ other_versions/Tree_screenshots/simple_tree_screenshots.lua
@@ -0,0 +1,270 @@
+--This script is a simple documentation of a tree in a Lua table
+
+
+--1. basic data
+
+--1.1 libraries
+require('iuplua')           --require iuplua for GUIs
+--1.1.1 import libraries for images
+require("cdlua")
+require("cdluapdf") --for pdf and powerpoint
+require("imlua")
+require("cdluaim") --for screen capture
+require("iuplua")
+require("iupluacd") --for iup canvas
+
+
+--1.3 initalize clipboard
+clipboard=iup.clipboard{}
+
+--1.2 color section
+--1.2.1 color of the console associated with the graphical user interface if started with lua54.exe and not wlua54.exe
+os.execute('color 71')
+
+--1.2.2 Beckmann und Partner colors
+color_red_bpc="135 31 28"
+color_light_color_grey_bpc="196 197 199"
+color_grey_bpc="162 163 165"
+color_blue_bpc="18 32 86"
+
+--1.2.3 color definitions
+color_background=color_light_color_grey_bpc
+color_buttons=color_blue_bpc -- works only for flat buttons, "18 32 86" is the blue of BPC
+color_button_text="255 255 255"
+color_background_tree="246 246 246"
+
+
+--1.3 path of the graphical user interface and filename of this script
+path=arg[0]:match("(.*)\\")
+--test with: print(path)
+thisfilename=arg[0]:match("\\([^\\]+)$")
+--test with: print(arg[0])
+--test with: print(thisfilename)
+
+path_documentation_tree=path .. "\\" .. "screenshot_tree.lua"
+
+
+--3 functions
+
+--3.1 general lua-functions
+
+--3.1.1 function checking if file exits
+function file_exists(name)
+   local f=io.open(name,"r")
+   if f~=nil then io.close(f) return true else return false end
+end --function file_exists(name)
+
+--3.1.2 function escaping magic characters
+function string.escape_forbidden_char(insertstring) --this function takes a string and returns the same string with escaped characters
+	return insertstring:gsub("\\", "\\\\"):gsub("\"", "\\\""):gsub("\'", "\\\'"):gsub("\n", "\\n"):gsub("\r", "\\n")
+end --function string.escape_forbidden_char(insertstring)
+
+
+
+
+--5. context menus (menus for right mouse click)
+
+--5.1 menu of tree
+--5.1.1 copy node of tree
+startcopy = iup.item {title = "Knoten kopieren"}
+function startcopy:action() --copy node
+	 clipboard.text = tree['title']
+end --function startcopy:action()
+
+--5.1.2 start the file or repository of the node of tree 
+startnode = iup.item {title = "Starten"}
+function startnode:action() 
+	if tree['title']:match("^.:\\.*%.[^\\ ]+$") or tree['title']:match("^.:\\.*[^\\]+$") or tree['title']:match("^.:\\$") or tree['title']:match("^[^ ]*//[^ ]+$") then 
+		os.execute('start "D" "' .. tree['title'] .. '"') 
+	elseif tree['title']:match("sftp .*") then 
+		os.execute(tree['title']) 
+	end --if tree['title']:match("^.:\\.*%.[^\\ ]+$") or tree['title']:match("^.:\\.*[^\\]+$") or tree['title']:match("^.:\\$") or tree['title']:match("^[^ ]*//[^ ]+$") then 
+end --function startnode:action()
+
+
+
+--5.1.3 put the buttons together in the menu for tree
+menu = iup.menu{
+		startcopy,
+		startnode, 
+		}
+--5.1 menu of tree end
+
+
+--5. context menus (menus for right mouse click) end
+
+
+--6 buttons
+--6.1 logo image definition and button wiht logo 
+img_logo = iup.image{
+  { 4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4 }, 
+  { 4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4,4,4 }, 
+  { 4,1,1,1,1,1,1,1,1,1,3,3,1,1,3,3,3,1,1,1,1,1,3,1,1,1,3,1,1,1,1,4,4,4 }, 
+  { 4,1,1,1,1,1,1,1,1,1,3,3,1,1,3,1,1,3,1,1,1,1,3,1,1,3,1,1,1,1,1,4,4,4 }, 
+  { 4,1,1,1,1,3,3,3,3,1,1,1,1,1,3,1,1,3,1,1,1,1,3,1,3,1,1,1,1,1,1,4,4,4 }, 
+  { 4,1,1,1,3,3,3,4,4,3,1,1,1,1,3,3,3,3,1,1,1,1,3,3,1,1,1,1,1,1,1,4,4,4 }, 
+  { 4,1,1,3,3,3,3,4,4,3,3,1,1,1,3,1,1,1,3,1,1,1,3,1,3,1,1,1,1,1,1,4,4,4 }, 
+  { 4,1,1,3,3,3,3,3,3,3,3,1,1,1,3,1,1,1,3,1,1,1,3,1,1,3,1,1,1,1,1,4,4,4 }, 
+  { 4,1,1,3,3,3,3,3,3,3,3,1,1,1,3,3,3,3,1,1,3,1,3,1,1,1,3,1,3,1,1,4,4,4 }, 
+  { 4,1,1,1,3,3,3,3,3,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4,4,4 }, 
+  { 4,1,1,1,1,3,3,3,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4,4,4 }, 
+  { 4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,4,4,4 }, 
+  { 4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,3,1,3,1,1,1,1,1,1,1,4,4,4 }, 
+  { 4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,3,1,3,1,1,1,1,1,1,4,4,4 }, 
+  { 4,1,1,1,1,1,1,1,1,1,1,1,1,1,4,1,1,1,1,1,3,1,3,3,1,1,1,1,1,1,1,4,4,4 }, 
+  { 4,1,1,1,1,1,1,1,1,1,1,1,1,4,4,4,4,1,1,3,3,1,3,1,3,1,1,1,1,1,1,4,4,4 }, 
+  { 4,1,1,1,1,1,1,1,1,1,1,4,4,4,4,4,4,4,1,1,3,3,1,3,1,1,1,1,1,1,1,4,4,4 }, 
+  { 4,1,1,1,1,1,1,1,1,4,4,4,4,4,3,3,4,4,4,4,1,3,3,1,1,1,1,1,1,1,4,4,4,4 },
+  { 4,1,1,1,1,1,1,1,4,4,4,4,3,3,3,3,3,3,4,4,4,3,1,1,1,1,1,1,1,1,1,4,4,4 },
+  { 4,1,1,1,1,1,4,4,4,4,4,3,3,3,3,3,3,3,3,3,4,3,4,1,1,1,1,1,1,1,1,4,4,4 },
+  { 4,1,1,1,1,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,4,4,4,1,1,1,1,1,1,4,4,4 },
+  { 4,1,1,4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4,4,1,1,1,1,1,4,4,4 }, 
+  { 4,4,4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4,4,1,1,1,4,4,4 }, 
+  { 4,4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4,1,1,4,4,4 }, 
+  { 4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,1,4,4,4 }, 
+  { 4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4,4,4 }, 
+  { 4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4,4 },  
+  { 4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4 },  
+  { 4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3 },  
+  { 4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4,4 },  
+  { 3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,4 },  
+  { 3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4 },  
+  { 3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4 },  
+  { 4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4 }  
+  ; colors = { color_grey_bpc, color_light_color_grey_bpc, color_blue_bpc, "255 255 255" }
+}
+button_logo=iup.button{image=img_logo,title="", size="23x20"}
+function button_logo:action()
+	iup.Message("Beckmann & Partner CONSULT","BERATUNGSMANUFAKTUR\nMeisenstraße 79\n33607 Bielefeld\nDr. Bruno Kaiser\nLizenz Open Source")
+end --function button_logo:flat_action()
+
+--6.2 button for screen capture
+button_screenshot=iup.flatbutton{title="Screenshot herstellen", size="95x20", BGCOLOR=color_buttons, FGCOLOR=color_button_text}
+function button_screenshot:flat_action()
+	--screen capture
+	--part of screen capture: canvas=cd.CreateCanvas(cd.NATIVEWINDOW,nil)
+	canvas=cd.CreateCanvas(cd.NATIVEWINDOW,nil)
+	canvas:Activate()
+	w,h=canvas:GetSize()
+	image=im.ImageCreate(w*0.8,h,im.RGB,im.BYTE)
+	image:cdCanvasGetImage(canvas,0,0)
+	if tree['TITLE']:lower():match("%.png") then
+		image:Save(tree['TITLE'],"PNG") --.jpg, "JPG" --.png, "PNG" --.bmp,"BMP"
+	else
+		image:Save("screenshot.png","PNG") --.jpg, "JPG" --.png, "PNG" --.bmp,"BMP"
+	end --if tree['TITLE']:lower():match("%.png") then
+	canvas:Kill()
+end --function button_screenshot:flat_action()
+
+--6.10 button with second logo
+button_logo2=iup.button{image=img_logo,title="", size="23x20"}
+function button_logo2:action()
+	iup.Message("Beckmann & Partner CONSULT","BERATUNGSMANUFAKTUR\nMeisenstraße 79\n33607 Bielefeld\nDr. Bruno Kaiser\nLizenz Open Source")
+end --function button_logo:flat_action()
+
+--6 buttons end
+
+
+--7 Main Dialog
+--7.1 load tree from file (this ensures that tree and tree2 are compared contentwise)
+if file_exists(path_documentation_tree) then
+	dofile(path_documentation_tree) --initialize the tree, read from the lua file
+	for line in io.lines(path_documentation_tree) do
+		if line:match('=')~= nil then 
+			tablename=line:sub(1,line:find('=')-1):gsub(' ', '')
+			break
+		end --if line:match('=')~= nil then 
+	end --for line in io.lines(path_documentation_tree) do
+	--save table in the variable actualtree
+	--Lua 5.1 has the function loadstring() - in later versions, this is replaced by load(), hence we detect this here
+	if _VERSION=='Lua 5.1' then
+		loadstring('actualtree='..tablename)()
+	else
+		load('actualtree='..tablename)() --now actualtree is the table.
+	end --if _VERSION=='Lua 5.1' then
+end --if file_exists(path_documentation_tree) then
+--builde tree
+tree=iup.tree{
+map_cb=function(self)
+self:AddNodes(actualtree)
+end, --function(self)
+SIZE="100x100",
+showrename="YES",--F2 key active
+markmode="SINGLE",--for Drag & Drop SINGLE not MULTIPLE
+showdragdrop="YES",
+}
+--set colors of tree
+tree.BGCOLOR=color_background_tree --set the background color of the tree
+-- Callback of the right mouse button click
+function tree:rightclick_cb(id)
+	tree.value = id
+	menu:popup(iup.MOUSEPOS,iup.MOUSEPOS) --popup the defined menue
+end --function tree:rightclick_cb(id)
+-- Callback called when a node will be doubleclicked
+function tree:executeleaf_cb(id)
+	if tree['title' .. id]:match("^.:\\.*%.[^\\ ]+$") or tree['title' .. id]:match("^.:\\.*[^\\]+$") or tree['title' .. id]:match("^.:\\$") or tree['title' .. id]:match("^[^ ]*//[^ ]+$") then os.execute('start "d" "' .. tree['title' .. id] .. '"') end
+end --function tree:executeleaf_cb(id)
+-- Callback for pressed keys
+function tree:k_any(c)
+	if c == iup.K_DEL then
+		-- do a totalchildcount of marked node. Then pop the table entries, which correspond to them.
+		for j=0,tree.totalchildcount do
+			--table.remove(attributes, tree.value+1)
+		end --for j=0,tree.totalchildcount do
+		tree.delnode = "MARKED"
+	elseif c == iup.K_cP then -- added output of current table to a text file
+		printtree()
+	elseif c == iup.K_cR then -- expand collapse dialog
+		text_expand_collapse.value=tree.title
+		dlg_expand_collapse:popup(iup.ANYWHERE, iup.ANYWHERE)
+	elseif c == iup.K_cT then -- alphabetic tree sort
+		alphabetic_tree_sort(tree)
+	elseif c == iup.K_cF then
+			searchtext.value=tree.title
+			searchtext.SELECTION="ALL"
+			dlg_search:popup(iup.ANYWHERE, iup.ANYWHERE)
+	elseif c == iup.K_Menu then
+		menu:popup(iup.MOUSEPOS,iup.MOUSEPOS) --popup the defined menue
+	end --if c == iup.K_DEL then
+end --function tree:k_any(c)
+
+
+--7.5 building the dialog and put buttons, trees and preview together
+maindlg = iup.dialog{
+	--simply show a box with buttons
+	iup.vbox{
+		--first row of buttons
+		iup.hbox{
+			button_logo,
+			button_screenshot,
+			iup.label{size="10x",},
+			iup.fill{},
+			button_logo2,
+		},
+		
+		iup.hbox{
+			iup.frame{title="Manuelle Zuordnung als Baum",tree,},
+			},
+
+	},
+
+	icon = img_logo,
+	title = path .. " Documentation Tree",
+	SIZE = '220xFULL',
+	BACKGROUND=color_background
+}
+
+--7.5.1 show the dialog
+maindlg:showxy(iup.RIGHT,iup.CENTER)
+
+--7.5.3 go to the main dialog
+iup.NextField(maindlg)
+
+
+
+
+--7.8 Main Loop
+if (iup.MainLoopLevel()==0) then
+	iup.MainLoop()
+end --if (iup.MainLoopLevel()==0) then
+
