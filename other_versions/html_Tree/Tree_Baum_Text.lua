790635ae2e0b20cc0ad0228b2b1be9e800986160
BrunoKaiser <64803261+BrunoKaiser@users.noreply.github.com>
2022-02-21 16:17:32 +0100

    add file Tree_Baum_Text.lua
    add file Tree_Baum_Text.lua

---
+++ other_versions/html_Tree/Tree_Baum_Text.lua
@@ -0,0 +1,26 @@
+Tree = 
+
+
+{branchname="Title\\n\\n",
+
+{branchname="Subtitle\\n\\n",
+"This is a text.\\n\\n",
+"This is a line but without ",
+"linebreak. The line is ",
+"broken after this text.\\n\\n",
+},
+
+
+{branchname="Next subtitle\\n\\n",
+"This is a text.\\n\\n",
+"This is a line but without ",
+"linebreak. The line is ",
+"broken after this text.\\n\\n",
+},
+
+
+
+}
+
+
+return Tree
