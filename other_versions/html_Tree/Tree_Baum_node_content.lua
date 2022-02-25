fb4e52e95881ff19410b495f46f3fb168862b322
BrunoKaiser <64803261+BrunoKaiser@users.noreply.github.com>
2022-02-25 13:05:45 +0100

    add file Tree_Baum_node_content.lua
    add file Tree_Baum_node_content.lua

---
+++ other_versions/html_Tree/Tree_Baum_node_content.lua
@@ -0,0 +1,24 @@
+Tree = 
+
+{ branchname="example of a reflexive tree", 
+
+{ branchname="IUP-Lua Hilfe", 
+
+ "https://webserver2.tecgraf.puc-rio.br/iup/",
+{ branchname="Controls", 
+{ branchname="Standard", 
+
+ "IupTree",},
+{ branchname="Additional", 
+
+ "IupWebBrowser",},
+},
+},
+
+}--lua_tree_output
+
+
+
+
+
+return Tree
