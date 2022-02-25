f8102ea1717bc53e5308897cae12610e4e4fefa7
BrunoKaiser <64803261+BrunoKaiser@users.noreply.github.com>
2022-02-25 13:05:15 +0100

    add file html_build_node_content.lua
    add file html_build_node_content.lua

---
+++ other_versions/html_Tree/html_build_node_content.lua
@@ -0,0 +1,67 @@
+
+dofile("C:\\Tree\\html_Tree\\Tree_Baum_node_content.lua")
+--test with: print(Tree[1][1][2])
+outputfile1=io.open("C:\\Tree\\html_Tree\\Tree_Baum_node_content.html","w")
+outputfile1:write('<font size="5"> ')
+function readTreetohtmlRecursive(TreeTable)
+	if TreeTable.branchname:match("^http")==nil then
+	AusgabeTabelle[TreeTable.branchname]=TreeTable.branchname .. " ("
+	end --if TreeTable.branchname:match("^http")==nil then
+	for k,v in ipairs(TreeTable) do
+		if type(v)=="table" then
+			if TreeTable.branchname:match("^http")==nil and v.branchname:match("^http")==nil then
+				AusgabeTabelle[TreeTable.branchname]=AusgabeTabelle[TreeTable.branchname] .. v.branchname .. ", "
+			end --if TreeTable.branchname:match("^http")==nil then
+		else
+			if TreeTable.branchname:match("^http")==nil and v:match("^http")==nil then
+				AusgabeTabelle[TreeTable.branchname]=AusgabeTabelle[TreeTable.branchname] .. v .. ", "
+			end --if TreeTable.branchname:match("^http")==nil then
+		end --if type(v)=="table" then
+	end --for k, v in ipairs(TreeTable) do
+	--write modified or unmodified branchname
+	if AusgabeTabelle[TreeTable.branchname] and AusgabeTabelle[TreeTable.branchname]:match("%($")==nil then
+		outputfile1:write("<ul><li>" ..
+		tostring(AusgabeTabelle[TreeTable.branchname]:gsub(", $",")"):gsub("([^%(]*)(%(.*%))","<b>%1</b><i>%2</i>"))
+		:gsub("ä","&auml;")
+		:gsub("Ä","&Auml;")
+		:gsub("ö","&ouml;")
+		:gsub("Ö","&Ouml;")
+		:gsub("ü","&uuml;")
+		:gsub("Ü","&Uuml;")
+		:gsub("ß","&szlig;")
+		.. "\n")
+	else
+		outputfile1:write("<ul><li>" ..
+		tostring(TreeTable.branchname)
+		:gsub("ä","&auml;")
+		:gsub("Ä","&Auml;")
+		:gsub("ö","&ouml;")
+		:gsub("Ö","&Ouml;")
+		:gsub("ü","&uuml;")
+		:gsub("Ü","&Uuml;")
+		:gsub("ß","&szlig;")
+		.. "\n")
+	end --if AusgabeTabelle[TreeTable.branchname]:match("%($")==nil then
+	--go to the recursion
+	for k,v in ipairs(TreeTable) do
+		if type(v)=="table" then
+			readTreetohtmlRecursive(v)
+		else
+			outputfile1:write("<ul><li>" .. v
+			:gsub("ä","&auml;")
+			:gsub("Ä","&Auml;")
+			:gsub("ö","&ouml;")
+			:gsub("Ö","&Ouml;")
+			:gsub("ü","&uuml;")
+			:gsub("Ü","&Uuml;")
+			:gsub("ß","&szlig;")
+			.. "</li></ul>" .. "\n")
+		end --if type(v)=="table" then
+	end --for k, v in ipairs(TreeTable) do
+	outputfile1:write("</li></ul>" .. "\n")
+end --readTreetohtmlRecursive(TreeTable)
+  
+--apply the recursive function
+AusgabeTabelle={}
+readTreetohtmlRecursive(Tree)
+outputfile1:close()
