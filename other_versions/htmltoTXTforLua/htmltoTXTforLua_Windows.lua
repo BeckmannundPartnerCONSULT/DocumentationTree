58e94250595ff18b336cfc689ff4bb3158b002c4
BrunoKaiser <64803261+BrunoKaiser@users.noreply.github.com>
2022-03-24 14:53:21 +0100

    update htmltoTXTforLua_Windows.lua
    update htmltoTXTforLua_Windows.lua

--- other_versions/htmltoTXTforLua/htmltoTXTforLua_Windows.lua
+++ other_versions/htmltoTXTforLua/htmltoTXTforLua_Windows.lua
@@ -18,8 +18,8 @@ function treeCollectFileNamesRecursive(TreeTable)
 end --function treeCollectFileNamesRecursive(TreeTable)
 collectFileNamesTable={}
 treeCollectFileNamesRecursive(lua_tree_output)
-for k,v in pairs(collectFileNamesTable) do 
-	if k:match("https://") and k:match(" ")==nil then 
+for k,v in pairs(collectFileNamesTable) do
+	if k:match("https://") and k:match(" ")==nil then
 				URLTable[k]=k:gsub("https:","")
 						:gsub("%.","")
 						:gsub("/","~")
@@ -32,32 +32,32 @@ for k,v in pairs(collectFileNamesTable) do
 						:gsub(">","")
 						:gsub("|","")
 						:gsub("=","")
-	end --if k:match("https://") then 
-end --for k,v in pairs(collectFileNamesTable) do 
+	end --if k:match("https://") then
+end --for k,v in pairs(collectFileNamesTable) do
 --test with: for k,v in pairs(URLTable) do print(k, v) end
 
 --3. export internet files to text if there is no export
---If the html-format is not readable for word, for instance because frames are not supported by curl, then nothing happens. 
+--If the html-format is not readable for word, for instance because frames are not supported by curl, then nothing happens.
 fileTable={}
 numberOfLines=0
-for k,v in pairs(URLTable) do 
+for k,v in pairs(URLTable) do
 	local fileFound=""
 	--test with: print(v)
 	p=io.popen('dir C:\\Temp\\Archiv\\' .. v .. '_html2.txt /b/o/s')
-	for line in p:lines() do 
-		--test with: print(line) 
+	for line in p:lines() do
+		--test with: print(line)
 		fileFound=line
-	end --for line in p:lines() do 
+	end --for line in p:lines() do
 	if fileFound=="" then
 		numberOfLines=numberOfLines+1
 		fileTable[k]=v
 	end --if fileFound~="" then
-end --for k,v in pairs(URLTable) do 
+end --for k,v in pairs(URLTable) do
 print("Number of sites to be exported to text file: " .. numberOfLines)
-for k,v in pairs(fileTable) do  
+for k,v in pairs(fileTable) do
 local URLText=k
 print(v)
---curl SysWOW64 kann mit System32 ersetzt werden, wohl für eine 32-bit-Version
+--curl SysWOW64 kann mit System32 ersetzt werden, wohl fÃ¼r eine 32-bit-Version
 os.execute('C:\\Windows\\SysWOW64\\curl.exe ' .. URLText .. ' --output C:\\temp\\test.html')
 word=luacom.CreateObject("Word.Application")
 openResult=pcall(function() word.Documents:Open("C:\\temp\\test.html") end)
@@ -65,5 +65,5 @@ if openResult then
 word.ActiveDocument:SaveAs2("C:\\Temp\\Archiv\\" .. v .. "_html2.txt",2)
 end --if openResult then
 word:Quit()
-end --for k,v in pairs(fileTable) do 
+end --for k,v in pairs(fileTable) do
 
